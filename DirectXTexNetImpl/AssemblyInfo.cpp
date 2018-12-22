8f03033c89f68c3c29a6919da3ecec847dd14210
deng0 <dengo@gmx.net>
2018-12-22 11:45:31 -0600

    - updated DirectXTex to November 16, 2018 Version
    - updated to VS 2017

--- DirectXTexNetImpl/AssemblyInfo.cpp
+++ DirectXTexNetImpl/AssemblyInfo.cpp
@@ -31,8 +31,8 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersion("1.0.0.2")];
-[assembly:AssemblyInformationalVersion("1.0.0-rc2")]
+[assembly:AssemblyVersion("1.0.0.3")];
+[assembly:AssemblyInformationalVersion("1.0.0-rc3")]
 
 [assembly:ComVisible(false)];
 
